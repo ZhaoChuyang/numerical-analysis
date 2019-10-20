31adf304c5ef87e57c3e874a6608db0ef3665642
chuyang <zcy201420752@gmail.com>
2019-10-20 16:49:07 +0800

    clean

--- bin/run_exp2.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# run experiment 2
-python -m src.exp2 1 --a 0 --b 1 --tol 0.0005 --n 100
-python -m src.exp2 2 --tol 0.00001 --n 50
-python -m src.exp2 3 --a 0 --b 1 --tol 0.00001 --n 50 --p0 0.5 --p1 0.6
