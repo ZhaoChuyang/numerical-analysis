1965cebf4b10b5478d8eed8ff9a226d2a9d1654c
chuyang <zcy201420752@gmail.com>
2019-11-25 20:10:53 +0800

    实验三完成

--- src/exp3.py
+++ src/exp3.py
@@ -3,19 +3,43 @@ import argparse
 
 import numpy as np
 
-from .utils.matrix import gaussian_elimination, lu_factorization
+from .utils.matrix import gaussian_elimination, lu_factorization, backward_substitution, forward_substitution, jacobi, gauss_seidel
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--method', help='choose from LU, Jacobi and Gauss-Seidel')
+    parser.add_argument('--norm', default='euclidean', help='choose from euclidean and maximum')
+    return parser.parse_args()
 
 
 def main():
+    args = get_args()
+    A = np.array([(4, -1, 1), (4, -8, 1), (-2, 1, 5)])
+    y = np.array([7, -21, 15])
+    print(f'shape of A is {A.shape}, shape of y is {y.shape}')
+    norm = args.norm
 
-    np.set_printoptions(suppress=True, precision=2)
-    # a = np.array([(4, -1, 1, 7), (4, -8, 1, -21), (-2, 1, 5, 15)], dtype=float)
-    a = np.random.random((9, 9))
-    a1 = gaussian_elimination(a)
-    l, u = lu_factorization(a)
+    if args.method == 'LU':
+        l_mat, u_mat = lu_factorization(A)
+        print(f'lower matrix of A is:\n{l_mat}')
+        print(f'upper matrix of A is:\n{u_mat}')
 
-    print(a)
-    print(np.matmul(l, u))
+        temp = forward_substitution(l_mat, y)
+        x = backward_substitution(u_mat, temp)
+
+        print(f"使用LU分解法，求得该线性方程组的解为 {x}")
+
+    if args.method == 'Jacobi':
+        x = jacobi(A, y, norm=norm, N=30, tol=1e-4)
+        print(f"使用Jacobi方法，求得该线性方程组的解为 {x[1]}，迭代次数为 {x[0]}")
+
+    if args.method == 'Gauss-Seidel':
+        x = gauss_seidel(A, y, norm=norm, N=30, tol=1e-4)
+        print(f"使用Gauss-Seidel方法，求得该线性方程组的解为 {x[1]}，迭代次数为 {x[0]}")
+
+    if args.method not in ['LU', 'Jacobi', 'Gauss-Seidel']:
+        print('请在LU, Jacobi, Gauss-Seidel中选择一种方法求解')
 
 
 if __name__ == '__main__':
--- src/utils/matrix.py
+++ src/utils/matrix.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import fabs
 
 
 def gaussian_elimination(a):
@@ -24,41 +25,137 @@ def gaussian_elimination(a):
 
 def lu_factorization(a):
 
-    n = np.size(a, 0)
+    n = len(a)
     mat_l = np.eye(n)
     mat_u = np.zeros((n, n))
 
-    def mat_mul(i, j):
-        sum = 0
-        # for k range from i to n-1 the val of l[i, k] is 0
-        for k in range(n): # or in range(n)
-            sum += mat_l[i, k] * mat_u[k, j]
-        return sum
-
     # Step 1
-    mat_u[0, 0] = a[0, 0] / mat_l[0, 0]
+    mat_u[0, 0] = a[0, 0]
     if mat_u[0, 0] == 0:
         raise Exception('不存在LU分解')
 
     # Step 2
-    for j in range(0, n):
+    for j in range(1, n):
         mat_u[0, j] = a[0, j]
         mat_l[j, 0] = a[j, 0] / mat_u[0, 0]
 
     # Step 3
     for i in range(1, n-1):
         # Step 4
-        mat_u[i, i] = a[i, i] - mat_mul(i, i)
+        mat_u[i][i] = a[i][i] - np.matmul(mat_l[i], mat_u[:, i])
+
         if mat_u[i, i] == 0:
             raise Exception('不存在LU分解')
+
         # Step 5
         for j in range(i+1, n):
-            mat_u[i, j] = a[i, j] - mat_mul(i, j)
-            mat_l[j, i] = a[j, i] - mat_mul(j, i)
+            mat_u[i, j] = a[i, j] - np.matmul(mat_l[i], mat_u[:, j])
+            mat_l[j, i] = (a[j, i] - np.matmul(mat_l[j], mat_u[:, i])) / mat_u[i][i]
 
     # Step 6
-    mat_u[n-1, n-1] = a[n-1, n-1] - mat_mul(n-1, n-1)
+    mat_u[n-1, n-1] = a[n-1, n-1] - np.matmul(mat_l[n-1], mat_u[:, n-1])
 
     return mat_l, mat_u
+
+
+# shape of y is (1,n), shape of a is (n,n), shape of x is (1,n)
+def backward_substitution(a, y):
+
+    n = len(y)
+    x = np.zeros(n)
+
+    if a.shape[0] != y.shape[0]:
+        raise Exception('矩阵和目标向量行数不相等')
+
+    x[n-1] = y[n-1] / a[n-1, n-1]
+
+    for i in range(n-2, -1, -1):
+        temp = 0
+        for j in range(n-1, i, -1):
+            temp += a[i][j] * x[j]
+        x[i] = (y[i] - temp) / a[i][i]
+
+    return x
+
+
+def forward_substitution(a, y):
+
+    n = a.shape[0]
+    x = np.zeros(n)
+
+    x[0] = y[0] / a[0][0]
+    for row in range(1, n):
+        temp = 0
+        for j in range(0, row):
+            temp += a[row][j] * x[j]
+
+        x[row] = (y[row] - temp) / a[row][row]
+
+    return x
+
+
+# shape of a is np.array(n, n), shape of b is np.array(n, 1), shape of xo is np.array(1, n)
+# tol is the tolerance, N is the maximum number of iterations, norm means which norm to use
+def jacobi(a, b, xo=None, tol=1e-6, N=100, norm='maximum'):
+    k = 1
+    n = a.shape[1]
+    if xo is None:
+        xo = np.zeros(n)
+    nxt_x = np.zeros(shape=n)
+    if norm == 'maximum':
+        def norm(a, b):
+            return np.max(np.abs(a-b))
+    if norm == 'euclidean':
+        def norm(a, b):
+            return np.sum((a-b) ** 2) ** (1/2)
+
+    while k <= N:
+
+        for i in range(0, n):
+
+            temp = 0
+            for j in range(n):
+                if i != j:
+                    temp += a[i][j] * xo[j]
+            nxt_x[i] = (b[i] - temp) / a[i][i]
+
+        if norm(nxt_x, xo) < tol:
+            return k, nxt_x
+
+        k += 1
+        xo = nxt_x.copy()
+
+
+def gauss_seidel(a, b, tol=1e-6, xo=None, N=100, norm='euclidean'):
+    k = 1
+    n = len(b)
+    x = np.zeros(n)
+    if xo is None:
+        xo = np.zeros(n)
+
+    if norm == 'euclidean':
+        def norm(x, y):
+            return np.sum((x - y) ** 2) ** (1/2)
+    if norm == 'maximum':
+        def norm(x, y):
+            return np.max(np.abs(x - y))
+
+    while k <= N:
+        for i in range(n):
+            temp = 0
+            for j in range(n):
+                if j < i:
+                    temp += x[j] * a[i][j]
+                if j > i:
+                    temp += xo[j] * a[i][j]
+
+            x[i] = (b[i] - temp) / a[i][i]
+
+        if norm(x, xo) <= tol:
+            return k, x
+
+        k += 1
+        xo = x.copy()
+
 
 
